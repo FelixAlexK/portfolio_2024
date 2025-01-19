a96b4fbfb6d1bdab639f7b4e4cbd0b6865506813
Felix.K <107888450+FelixAlexK@users.noreply.github.com>
2025-01-20 00:41:54 +0100

    refactor: simplify navigation target visibility handling by removing intersection observer logic

--- src/components/NavComponent.vue
+++ src/components/NavComponent.vue
@@ -1,40 +1,20 @@
 <script setup lang="ts">
 import {
-    useIntersectionObserver,
+
     useWindowScroll,
-    type MaybeElement,
+    
 } from "@vueuse/core";
-import { ref, type Ref } from "vue";
+
 
 const { homeTarget, aboutTarget, contactTarget } = defineProps<{
-    homeTarget: Ref<MaybeElement>;
-    aboutTarget: Ref<MaybeElement>;
-    contactTarget: Ref<MaybeElement>;
+    homeTarget: boolean;
+    aboutTarget: boolean;
+    contactTarget: boolean;
 }>();
-
-
-const homeTargetIsVisible = ref(false);
-const aboutTargetIsVisible = ref(false);
-const contactTargetIsVisible = ref(false);
-
-function setupIntersectionObserver(
-    target: Ref<MaybeElement, MaybeElement>,
-    visibilityFlag: Ref<boolean, boolean>,
-) {
-    useIntersectionObserver(
-        target,
-        ([entry]) => {
-            visibilityFlag.value = entry?.isIntersecting || false;
-        },
-        { threshold: 0.5 },
-    );
-}
 
 const { y } = useWindowScroll();
 
-setupIntersectionObserver(homeTarget, homeTargetIsVisible);
-setupIntersectionObserver(aboutTarget, aboutTargetIsVisible);
-setupIntersectionObserver(contactTarget, contactTargetIsVisible);
+
 </script>
 
 <template>
@@ -50,15 +30,13 @@ setupIntersectionObserver(contactTarget, contactTargetIsVisible);
         <nav>
             <ul class="flex items-center gap-12 font-rubik text-lg font-semibold">
                 <li>
-                    <a :class="{ 'text-blue-500': homeTargetIsVisible }" class="px-6 hover:text-blue-500"
-                        href="#home">Home</a>
+                    <a :class="{ 'text-blue-500': homeTarget }" class="px-6 hover:text-blue-500" href="#">Home</a>
                 </li>
                 <li>
-                    <a :class="{ 'text-blue-500': aboutTargetIsVisible }" class="px-6 hover:text-blue-500"
-                        href="#about">Ich</a>
+                    <a :class="{ 'text-blue-500': aboutTarget }" class="px-6 hover:text-blue-500" href="#about">Ich</a>
                 </li>
                 <li>
-                    <a :class="{ 'text-blue-500': contactTargetIsVisible }" class="px-6 hover:text-blue-500"
+                    <a :class="{ 'text-blue-500': contactTarget }" class="px-6 hover:text-blue-500"
                         href="#contact">Kontakt</a>
                 </li>
             </ul>
