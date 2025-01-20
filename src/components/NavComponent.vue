7c348dbda39ba001b428ccd8a5af795e9b2b1e31
Felix.K <107888450+FelixAlexK@users.noreply.github.com>
2025-01-20 21:49:47 +0100

    feat: update NavComponent with styling improvements and enhanced header text

--- src/components/NavComponent.vue
+++ src/components/NavComponent.vue
@@ -11,41 +11,25 @@ const { y } = useWindowScroll();
 </script>
 
 <template>
-  <div
-    :class="{
-      'bg-transparent text-gray-950': y <= 0,
-      'bg-gray-950 text-white': y > 5,
-    }"
-    class="flex h-24 w-full items-center justify-between border-b-2 border-blue-500 px-24 transition-colors duration-300"
-  >
-    <h1 class="cursor-pointer px-6 font-lato text-4xl font-bold">
-      <a href="#">Felix</a>
+  <div :class="{
+    'bg-transparent text-gray-950': y <= 0,
+    'bg-gray-950 text-white': y > 5,
+  }"
+    class="flex h-24 w-full items-center justify-between border-b-2 border-blue-500 px-24 transition-colors duration-300">
+    <h1 class="cursor-pointer px-6 font-lato text-4xl font-bold ">
+      <a href="#">FELIX</a>
     </h1>
+    <small class="font-rubik text-gray-100">2025</small>
     <nav>
       <ul class="flex items-center gap-12 font-rubik text-lg font-semibold">
         <li>
-          <a
-            :class="{ 'text-blue-500': homeTarget }"
-            class="px-6 hover:text-blue-500"
-            href="#"
-            >Home</a
-          >
+          <a :class="{ 'text-blue-500': homeTarget }" class="px-6 hover:text-blue-500" href="#">Home</a>
         </li>
         <li>
-          <a
-            :class="{ 'text-blue-500': aboutTarget }"
-            class="px-6 hover:text-blue-500"
-            href="#about"
-            >Ich</a
-          >
+          <a :class="{ 'text-blue-500': aboutTarget }" class="px-6 hover:text-blue-500" href="#about">Ich</a>
         </li>
         <li>
-          <a
-            :class="{ 'text-blue-500': contactTarget }"
-            class="px-6 hover:text-blue-500"
-            href="#contact"
-            >Kontakt</a
-          >
+          <a :class="{ 'text-blue-500': contactTarget }" class="px-6 hover:text-blue-500" href="#contact">Kontakt</a>
         </li>
       </ul>
     </nav>
