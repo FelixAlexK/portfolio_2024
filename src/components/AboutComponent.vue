790f8c49c61b7fcb04fa3ff254f787a89d7eeabd
Felix.K <107888450+FelixAlexK@users.noreply.github.com>
2025-01-20 21:49:36 +0100

    feat: update AboutComponent with new personal description and styling adjustments

--- src/components/AboutComponent.vue
+++ src/components/AboutComponent.vue
@@ -4,29 +4,42 @@
   <section class="mb-32 mt-32" ref="aboutTarget">
     <div class="flex w-full flex-row gap-24">
       <div class="flex w-1/3 flex-col">
-        <h2 class="mb-8 w-72 font-lato text-6xl font-semibold">
+        <h2 class="mb-8  font-lato text-6xl font-semibold">
           Hi, ich bin Felix
         </h2>
-        <img
-          src="../assets/AF7U4467b.jpg"
-          alt=""
-          class="mb-16 aspect-square w-80 rounded-3xl object-cover object-center"
-        />
+        <img src="../assets/AF7U4467b.jpg" alt=""
+          class="mb-16 aspect-square w-80 rounded-3xl object-cover object-center" />
         <div class="">
-          <a
-            href="#contact"
-            class="rounded-2xl bg-gray-950 px-16 py-4 text-lg font-medium text-white"
-            >Kontakt</a
-          >
+          <a href="#contact" class="rounded-2xl bg-gray-950 px-16 py-4 text-lg font-medium text-white">Kontakt</a>
         </div>
       </div>
-      <div class="w-2/3">
-        <span class="font-rubik text-lg"
-          >Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit
-          inventore tenetur iure ad voluptatum officia cumque incidunt,
-          praesentium architecto autem sapiente omnis mollitia eius, itaque
-          quidem molestias, repellat sunt? Sunt?</span
-        >
+      <div class="w-2/3 flex flex-col gap-16 mt-8">
+        <span class="font-rubik text-xl leading-8">Ich bin ein vielseitig interessierter Student mit einer großen
+          Begeisterung für Technik, Sport und Musik. Schon früh hat mich die Faszination für Technologie gepackt – ich
+          liebe es, innovative Lösungen zu entwickeln und neue Projekte anzupacken. Mein Angewandte
+          Informatik Studium ist
+          die perfekte Grundlage, um meine Leidenschaft für technologische Herausforderungen mit kreativem Denken zu
+          verbinden.</span>
+
+        <div class="flex flex-row gap-8">
+          <span class="font-rubik text-lg">
+            Neben meinem Studium spielt Sport eine zentrale Rolle in meinem Leben. Vor allem Tennis und Fußball
+            begeistern
+            mich, denn sie vereinen Wettbewerb, Teamgeist und körperliche Fitness auf einzigartige Weise. Die Energie
+            und
+            der Fokus, die ich auf dem Spielfeld erlebe, motivieren mich auch in anderen Bereichen meines Lebens, mein
+            Bestes zu geben.
+          </span>
+
+          <span class="font-rubik text-lg"> Ein weiteres Herzstück meines Lebens ist die Musik. Ich liebe den warmen,
+            unvergleichlichen Klang von
+            Schallplatten, die für mich mehr als nur Musik transportieren – sie erzählen Geschichten. Egal ob Klassiker
+            oder neue Künstler, ich finde Inspiration und Ruhe in diesen Klängen, die mich immer wieder begleiten.
+          </span>
+        </div>
+
+
+
       </div>
     </div>
   </section>
