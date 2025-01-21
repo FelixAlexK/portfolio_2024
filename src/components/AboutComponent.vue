e71c09073b223f0bb9d55f3dc92e2378802896e4
Felix.K <107888450+FelixAlexK@users.noreply.github.com>
2025-01-21 21:23:41 +0100

    feat: refine layout in AboutComponent with improved spacing and structure

--- src/components/AboutComponent.vue
+++ src/components/AboutComponent.vue
@@ -1,47 +1,54 @@
 <script setup lang="ts"></script>
 
 <template>
-  <section class="mb-32 mt-32" ref="aboutTarget">
-    <div class="flex w-full flex-col md:flex-row md:gap-24 gap-12">
-      <div class="flex md:w-1/3 flex-col w-full">
-        <h2 class="mb-8  font-lato md:text-6xl text-4xl  font-semibold">
+  <section class="" ref="aboutTarget">
+    <div class="flex w-full flex-col gap-12 md:flex-row md:gap-24">
+      <div class="flex w-full flex-col md:w-1/3">
+        <h2 class="mb-8 font-lato text-4xl font-semibold md:text-6xl">
           Hi, ich bin Felix
         </h2>
-        <img src="../assets/AF7U4467b.jpg" alt=""
-          class="md:mb-16 aspect-square w-80 rounded-3xl object-cover object-center  " />
+        <img
+          src="../assets/AF7U4467b.jpg"
+          alt=""
+          class="aspect-square w-80 rounded-3xl object-cover object-center md:mb-16"
+        />
         <div class="hidden md:block">
-          <a href="#contact" class="rounded-2xl bg-gray-950 px-16 py-4 text-lg font-medium text-white">Kontakt</a>
+          <a
+            href="#contact"
+            class="rounded-2xl bg-gray-950 px-16 py-4 text-lg font-medium text-white"
+            >Kontakt</a
+          >
         </div>
       </div>
-      <div class="md:w-2/3 w-full flex flex-col md:gap-16 gap-8 md:mt-8">
-        <span class="font-rubik md:text-2xl text-xl  leading-8">Ich bin ein vielseitig interessierter Student mit einer
-          großen
-          Begeisterung für Technik, Sport und Musik. Schon früh hat mich die Faszination für Technologie gepackt – ich
-          liebe es, innovative Lösungen zu entwickeln und neue Projekte anzupacken. Mein Angewandte
-          Informatik Studium ist
-          die perfekte Grundlage, um meine Leidenschaft für technologische Herausforderungen mit kreativem Denken zu
-          verbinden.</span>
+      <div class="flex w-full flex-col gap-8 md:mt-8 md:w-2/3 md:gap-16">
+        <span class="font-rubik text-xl leading-8 md:text-2xl"
+          >Ich bin ein vielseitig interessierter Student mit einer großen
+          Begeisterung für Technik, Sport und Musik. Schon früh hat mich die
+          Faszination für Technologie gepackt – ich liebe es, innovative
+          Lösungen zu entwickeln und neue Projekte anzupacken. Mein Angewandte
+          Informatik Studium ist die perfekte Grundlage, um meine Leidenschaft
+          für technologische Herausforderungen mit kreativem Denken zu
+          verbinden.</span
+        >
 
-        <div class="flex flex-col md:flex-row gap-8">
-          <span class="font-rubik md:text-lg text-base">
-            Neben meinem Studium spielt Sport eine zentrale Rolle in meinem Leben. Vor allem Tennis und Fußball
-            begeistern
-            mich, denn sie vereinen Wettbewerb, Teamgeist und körperliche Fitness auf einzigartige Weise. Die Energie
-            und
-            der Fokus, die ich auf dem Spielfeld erlebe, motivieren mich auch in anderen Bereichen meines Lebens, mein
-            Bestes zu geben.
+        <div class="flex flex-col gap-8 md:flex-row">
+          <span class="font-rubik text-base md:text-lg">
+            Neben meinem Studium spielt Sport eine zentrale Rolle in meinem
+            Leben. Vor allem Tennis und Fußball begeistern mich, denn sie
+            vereinen Wettbewerb, Teamgeist und körperliche Fitness auf
+            einzigartige Weise. Die Energie und der Fokus, die ich auf dem
+            Spielfeld erlebe, motivieren mich auch in anderen Bereichen meines
+            Lebens, mein Bestes zu geben.
           </span>
 
-          <span class="font-rubik md:text-lg text-base"> Ein weiteres Herzstück meines Lebens ist die Musik. Ich liebe
-            den warmen,
-            unvergleichlichen Klang von
-            Schallplatten, die für mich mehr als nur Musik transportieren – sie erzählen Geschichten. Egal ob Klassiker
-            oder neue Künstler, ich finde Inspiration und Ruhe in diesen Klängen, die mich immer wieder begleiten.
+          <span class="font-rubik text-base md:text-lg">
+            Ein weiteres Herzstück meines Lebens ist die Musik. Ich liebe den
+            warmen, unvergleichlichen Klang von Schallplatten, die für mich mehr
+            als nur Musik transportieren – sie erzählen Geschichten. Egal ob
+            Klassiker oder neue Künstler, ich finde Inspiration und Ruhe in
+            diesen Klängen, die mich immer wieder begleiten.
           </span>
         </div>
-
-
-
       </div>
     </div>
   </section>
