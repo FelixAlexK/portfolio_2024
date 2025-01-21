3905437f7dd3fa766fa5b4bf4c0cbfce0a88a111
Felix.K <107888450+FelixAlexK@users.noreply.github.com>
2025-01-21 21:23:45 +0100

    feat: improve layout and formatting in ContactComponent for better readability and structure

--- src/components/ContactComponent.vue
+++ src/components/ContactComponent.vue
@@ -1,16 +1,14 @@
 <script setup lang="ts">
 import { Copy, Loader2, Check } from "lucide-vue-next";
 import { ref, useTemplateRef } from "vue";
-import emailjs from '@emailjs/browser';
+import emailjs from "@emailjs/browser";
 
 const copied = ref(false);
-const formRef = useTemplateRef('form')
+const formRef = useTemplateRef("form");
 const isLoading = ref(false);
-
 
 let intervalIdCopying: number | undefined;
 let intervalIdEmail: number | undefined;
-
 
 function copyToClipboard() {
   navigator.clipboard
@@ -34,8 +32,6 @@ function copyToClipboard() {
   }, 3000);
 }
 
-
-
 interface FormData {
   name: string;
   email: string;
@@ -43,24 +39,29 @@ interface FormData {
 }
 
 const form = ref<FormData>({
-  name: '',
-  email: '',
-  message: '',
+  name: "",
+  email: "",
+  message: "",
 });
 
 const successMessage = ref<string | null>(null);
-const errorMessage = ref('');
+const errorMessage = ref("");
 
 function sendEmail() {
   isLoading.value = true;
   if (formRef.value) {
     emailjs
-      .sendForm(import.meta.env.VITE_EMAIL_SERVICE_ID, import.meta.env.VITE_EMAIL_TEMPLATE_ID, formRef.value, {
-        publicKey: import.meta.env.VITE_EMAIL_PUBLIC_KEY,
-      })
+      .sendForm(
+        import.meta.env.VITE_EMAIL_SERVICE_ID,
+        import.meta.env.VITE_EMAIL_TEMPLATE_ID,
+        formRef.value,
+        {
+          publicKey: import.meta.env.VITE_EMAIL_PUBLIC_KEY,
+        },
+      )
       .then(
         () => {
-          successMessage.value = 'Nachricht wurde erfolgreich versendet';
+          successMessage.value = "Nachricht wurde erfolgreich versendet";
           isLoading.value = false;
 
           if (intervalIdEmail) {
@@ -74,65 +75,83 @@ function sendEmail() {
           }, 3000);
         },
         (error) => {
-          console.log('FAILED...', error.text);
+          console.log("FAILED...", error.text);
           clearInterval(intervalIdEmail);
           intervalIdEmail = undefined;
           isLoading.value = false;
         },
       );
   } else {
-    console.error('Form reference is null');
+    console.error("Form reference is null");
     isLoading.value = false;
   }
-
 }
 </script>
 
 <template>
-  <section class="mb-32 mt-32" ref="contactTarget">
-    <div class="w-full rounded-xl bg-gray-100 md:p-16 p-4">
-      <h2 class="mb-1 font-lato md:text-6xl text-4xl font-bold ">Kontaktiere mich</h2>
-      <p class="mb-8 font-rubik md:text-lg text-base">
+  <section class="" ref="contactTarget">
+    <div class="w-full rounded-xl bg-gray-100 p-4 md:p-16">
+      <h2 class="mb-1 font-lato text-4xl font-bold md:text-6xl">
+        Kontaktiere mich
+      </h2>
+      <p class="mb-8 font-rubik text-base md:text-lg">
         Sende mir eine Nachricht und ich melde mich bei dir.
       </p>
       <form ref="form" @submit.prevent="sendEmail" class="flex flex-col">
+        <input
+          required
+          name="from_name"
+          type="text"
+          placeholder="Name*"
+          class="mb-4 h-16 w-full rounded-3xl px-6 font-rubik text-base focus-visible:shadow-xl focus-visible:outline-none md:text-lg"
+        />
+        <input
+          required
+          name="from_email"
+          type="email"
+          placeholder="Email*"
+          class="mb-4 h-16 w-full rounded-3xl px-6 font-rubik text-base focus-visible:shadow-xl focus-visible:outline-none md:text-lg"
+        />
+        <textarea
+          required
+          name="message"
+          type="text"
+          placeholder="Anfrage*"
+          class="mb-16 h-32 w-full rounded-3xl p-6 font-rubik text-base focus-visible:shadow-xl focus-visible:outline-none md:text-lg"
+        ></textarea>
 
-
-        <input required name="from_name" type="text" placeholder="Name*"
-          class="mb-4 h-16 w-full rounded-3xl px-6 font-rubik md:text-lg text-base focus-visible:shadow-xl focus-visible:outline-none" />
-        <input required name="from_email" type="email" placeholder="Email*"
-          class="mb-4 h-16 w-full rounded-3xl px-6 font-rubik md:text-lg text-base focus-visible:shadow-xl focus-visible:outline-none" />
-        <textarea required name="message" type="text" placeholder="Anfrage*"
-          class="mb-16 h-32 w-full rounded-3xl p-6 font-rubik md:text-lg text-base focus-visible:shadow-xl focus-visible:outline-none"></textarea>
-
-        <div class="flex flex-col justify-start items-start mb-16">
-
-          <button type="submit"
-            class=" mb-1 rounded-2xl bg-gray-950 px-16 py-4 font-rubik md:text-lg text-base font-medium text-white ">
+        <div class="mb-16 flex flex-col items-start justify-start">
+          <button
+            type="submit"
+            class="mb-1 rounded-2xl bg-gray-950 px-16 py-4 font-rubik text-base font-medium text-white md:text-lg"
+          >
             <Loader2 class="animate-spin" v-if="isLoading"></Loader2>
             <span v-else>Senden</span>
           </button>
-          <small v-if="successMessage" class=" font-rubik animate-pulse text-green-600">{{ successMessage }} </small>
-
+          <small
+            v-if="successMessage"
+            class="animate-pulse font-rubik text-green-600"
+            >{{ successMessage }}
+          </small>
         </div>
-
-
       </form>
 
-
-      <div class=" flex flex-row  items-center md:gap-4">
-
-        <span class="font-rubik md:text-2xl text-xl font-bold">felixk@iamfelixk.de
+      <div class="flex flex-row items-center md:gap-4">
+        <span class="font-rubik text-xl font-bold md:text-2xl"
+          >felixk@iamfelixk.de
         </span>
-        <button class="relative hidden md:block rounded px-2 py-1 hover:text-blue-500" @click="copyToClipboard">
+        <button
+          class="relative hidden rounded px-2 py-1 hover:text-blue-500 md:block"
+          @click="copyToClipboard"
+        >
           <Copy></Copy>
         </button>
-        <div v-if="copied"
-          class="relative   z-10 rounded bg-green-500 px-4 py-2 font-rubik md:text-sm text-xs text-white shadow-lg transition-transform duration-300">
+        <div
+          v-if="copied"
+          class="relative z-10 rounded bg-green-500 px-4 py-2 font-rubik text-xs text-white shadow-lg transition-transform duration-300 md:text-sm"
+        >
           <span>kopiert</span>
         </div>
-
-
       </div>
     </div>
   </section>
