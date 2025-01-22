4d5b2d6dd52e37efba5952ac4ce79a354336d37f
Felix.K <107888450+FelixAlexK@users.noreply.github.com>
2025-01-22 23:38:14 +0100

    fix: update image source in AboutComponent.vue for consistency

--- src/components/AboutComponent.vue
+++ src/components/AboutComponent.vue
@@ -7,29 +7,21 @@
         <h2 class="mb-8 font-lato text-4xl font-semibold md:text-6xl">
           Hi, ich bin Felix
         </h2>
-        <img
-          src="../assets/AF7U4467b.jpg"
-          alt=""
-          class="aspect-square w-80 rounded-3xl object-cover object-center md:mb-16"
-        />
+        <img src="../assets/IMG_1877.JPEG" alt=""
+          class="aspect-square w-80 rounded-3xl object-cover object-center md:mb-16" />
         <div class="hidden md:block">
-          <a
-            href="#contact"
-            class="rounded-2xl bg-gray-950 px-16 py-4 text-lg font-medium text-white"
-            >Kontakt</a
-          >
+          <a href="#contact" class="rounded-2xl bg-gray-950 px-16 py-4 text-lg font-medium text-white">Kontakt</a>
         </div>
       </div>
       <div class="flex w-full flex-col gap-8 md:mt-8 md:w-2/3 md:gap-16">
-        <span class="font-rubik text-xl leading-8 md:text-2xl"
-          >Ich bin ein vielseitig interessierter Student mit einer großen
+        <span class="font-rubik text-xl leading-8 md:text-2xl">Ich bin ein vielseitig interessierter Student mit einer
+          großen
           Begeisterung für Technik, Sport und Musik. Schon früh hat mich die
           Faszination für Technologie gepackt – ich liebe es, innovative
           Lösungen zu entwickeln und neue Projekte anzupacken. Mein Angewandte
           Informatik Studium ist die perfekte Grundlage, um meine Leidenschaft
           für technologische Herausforderungen mit kreativem Denken zu
-          verbinden.</span
-        >
+          verbinden.</span>
 
         <div class="flex flex-col gap-8 md:flex-row">
           <span class="font-rubik text-base md:text-lg">
