a829b2fd76367067694b24d8b45d817b5fca41a3
Felix Kuhbier <fkuhbier@stud.hs-heilbronn.de>
2025-02-26 22:57:43 +0100

    fix: enhance formatting and readability in MyPassionComponent.vue

--- src/components/MyPassionComponent.vue
+++ src/components/MyPassionComponent.vue
@@ -1,22 +1,18 @@
 <script setup lang="ts"></script>
 
 <template>
-  <section class="h-1/3">
+  <section v-once class="h-1/3">
     <div class="w-full">
       <div class="flex h-full flex-col-reverse gap-24 md:flex-row">
         <div class="flex flex-col md:w-1/2">
           <span class="mb-1 font-rubik text-xs text-blue-800 md:text-sm">Meine Leidenschaft</span>
-          <h2 class="mb-8 font-lato text-4xl font-semibold md:text-6xl">
-            Technik. Sport. Musik.
-          </h2>
-          <span class="font-rubik text-base md:mb-16 md:max-w-prose md:text-lg">Ich liebe es, meine Leidenschaften zu
-            verbinden und Menschen durch
-            Technologie, Sport und Musik zu inspirieren. Als technikbegeisterter
-            Student kombiniere ich Kreativität und Innovation, während ich in
-            meiner Freizeit beim Tennis oder Fußball Energie tanke. Mit meiner
-            Vorliebe für den warmen Klang von Schallplatten entdecke ich immer
-            wieder neue musikalische Welten, die mich berühren und
-            inspirieren.</span>
+          <h2 class="mb-8 font-lato text-4xl font-semibold md:text-6xl">Technik. Sport. Musik.</h2>
+          <span class="font-rubik text-base md:mb-16 md:max-w-prose md:text-lg"
+            >Ich liebe es, meine Leidenschaften zu verbinden und Menschen durch Technologie, Sport und Musik zu
+            inspirieren. Als technikbegeisterter Student kombiniere ich Kreativität und Innovation, während ich in
+            meiner Freizeit beim Tennis oder Fußball Energie tanke. Mit meiner Vorliebe für den warmen Klang von
+            Schallplatten entdecke ich immer wieder neue musikalische Welten, die mich berühren und inspirieren.</span
+          >
         </div>
         <div class="flex flex-col max-md:gap-16 md:w-1/2 md:justify-around">
           <div class="flex flex-col">
@@ -31,8 +27,9 @@
       </div>
     </div>
     <div class="hidden md:block">
-      <a href="#about" class="rounded-2xl bg-gray-950 px-16 py-4 font-rubik text-lg font-medium text-white">Das bin
-        ich</a>
+      <a href="#about" class="rounded-2xl bg-gray-950 px-16 py-4 font-rubik text-lg font-medium text-white"
+        >Das bin ich</a
+      >
     </div>
   </section>
 </template>
