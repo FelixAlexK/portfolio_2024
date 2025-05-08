57000aba46122519fced5cd3c4df1b56813b7d2d
Felix.K <107888450+FelixAlexK@users.noreply.github.com>
2025-05-08 13:22:17 +0200

    fix: remove Vuetify integration from main.ts for simplified setup

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,8 @@
 import { createApp } from "vue";
-import { createVuetify } from "vuetify";
-import * as components from "vuetify/components";
-import * as directives from "vuetify/directives";
 
 import App from "./app.vue";
 import "./style.css";
 
 const app = createApp(App);
 
-const vuetify = createVuetify({
-  components,
-  directives,
-});
-
-app.use(vuetify);
 app.mount("#app");
