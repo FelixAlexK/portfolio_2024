0b11b465b423f0a210068eb392807e6891c912af
Felix.K <107888450+FelixAlexK@users.noreply.github.com>
2025-05-12 18:07:51 +0200

    fix: update passion section for clarity and engagement

--- src/components/passion-component.vue
+++ src/components/passion-component.vue
@@ -15,28 +15,29 @@ import ButtonComponent from "./gradient-button-component.vue";
             Meine Leidenschaft
           </span>
           <h2 class="mb-6 font-lato text-4xl font-extrabold text-gray-900 md:text-5xl lg:text-6xl">
-            Technik. Sport. Musik.
+            Kreativität. Bewegung. Entdeckung.
           </h2>
           <p class="font-rubik text-base text-gray-700 md:mb-12 md:max-w-prose md:text-lg">
-            Ich liebe es, meine Leidenschaften zu verbinden und Menschen durch Technologie, Sport und Musik zu
-            inspirieren. Als technikbegeisterter Student kombiniere ich Kreativität und Innovation, während ich in
-            meiner Freizeit beim Tennis oder Fußball Energie tanke. Mit meiner Vorliebe für den warmen Klang von
-            Schallplatten entdecke ich immer wieder neue musikalische Welten, die mich berühren und inspirieren.
+            Ich bin Informatikstudent mit einer Leidenschaft für Technik, Sport und das Erkunden neuer Orte. Besonders begeistert mich die Webentwicklung, da sie Kreativität und technisches Denken perfekt vereint.
+
+            Neben dem Studium habe ich den Laufsport für mich entdeckt und trainiere mit dem Ziel, bald an Wettkämpfen teilzunehmen. Tennis und Fußball gehören ebenfalls zu meinen langjährigen Hobbys – der sportliche Ausgleich hält mich fokussiert und motiviert.
+
+            Reisen inspiriert mich, neue Kulturen, Menschen und Perspektiven kennenzulernen. Es erweitert nicht nur meinen Horizont, sondern bereichert auch meine Denkweise – ein wichtiger Aspekt, der mir im Berufsleben zugutekommt.
           </p>
           <ButtonComponent href="#about" text="Das bin ich" />
         </div>
 
         <!-- Right Section -->
-        <div class="flex flex-col items-center gap-12 md:w-1/2 md:justify-around">
-          <div class="flex flex-col items-center">
+        <div class="flex flex-col items-center gap-12 md:w-1/2 md:justify-center-safe ">
+          <div class="flex flex-col items-start">
             <span
-              class="mb-2 bg-gradient-to-tr from-blue-800 via-blue-600 to-blue-500 bg-clip-text text-6xl font-extrabold text-transparent"
+              class="mb-2 bg-gradient-to-tr from-blue-800 via-blue-600 to-blue-500 bg-clip-text text-6xl font-extrabold text-transparent text-nowrap"
             >
               22 Jahre
             </span>
             <span class="font-lato text-2xl text-gray-800">Alter</span>
           </div>
-          <div class="flex flex-col items-center">
+          <div class="flex flex-col items-start">
             <span
               class="mb-2 bg-gradient-to-tr from-blue-800 via-blue-600 to-blue-500 bg-clip-text text-6xl font-extrabold text-transparent"
             >
