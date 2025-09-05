50c3b0ac92413ef7f82a439700dd42328d8b7ac9
Felix Kuhbier <fkuhbier@stud.hs-heilbronn.de>
2025-09-05 04:06:56 +0200

    refactor: enhance home component structure and improve accessibility

--- src/components/home-component.vue
+++ src/components/home-component.vue
@@ -1,28 +1,30 @@
 <script setup lang="ts">
-import { Github, Linkedin } from "lucide-vue-next";
+import { ref } from "vue";
+
+const openToWork = ref(false);
 </script>
 
 <template>
-  <section class="">
+  <section id="home" class="">
     <div class="pb-40 pt-20 flex  items-center gap-40 max-lg:flex-col max-md:text-center">
       <div class="relative w-160 h-160 min-w-160 min-h-160 rounded-full overflow-hidden">
-        <div class="absolute inset-0 bg-gradient-to-r from-primary to-secondary rounded-full p-2">
-          <div class="w-full h-full bg-bg-dark rounded-full">
-            <img width="170" height="170" class="w-full h-full object-cover rounded-full border-b-bg-light border-2" src="../assets/IMG_1877.webp" alt="">
+        <div class="absolute inset-0 bg-gradient-to-r dark:from-primary dark:to-secondary rounded-full p-4">
+          <div class="w-full h-full rounded-full">
+            <img class="w-full h-full object-cover rounded-full border-2" src="../assets/IMG_1877.webp" alt="Profile Picture">
           </div>
         </div>
       </div>
       <div class="flex flex-col items-start  gap-28">
         <h1 class="dark:text-text-muted md:relative font-bold max-md:flex max-md:flex-col  max-md:items-center  text-3xl  tracking-tight  font-rubik">
           <span class="text-text">Hey, I'm Felix.</span>
-          <span> I'm a Frontend <br> Software Developer</span>
-          <div class="bg-success/20 w-full max-w-[120px] max-md:mt-12 md:mt-8 md:ml-8 md:absolute whitespace-nowrap text-success text-sm px-8 py-4 rounded-full inline-block  font-lato md:text-center">
+          <span> I'm a Frontend <br> Software Developer.</span>
+          <div v-if="openToWork" class="bg-success/20 w-full max-w-[120px] max-md:mt-12 md:mt-8 md:ml-8 md:absolute whitespace-nowrap text-success text-sm px-8 py-4 rounded-full inline-block  font-lato md:text-center">
             <span class="p-4 mb-px mr-2  inline-block bg-success rounded-full"></span>
             Open to work
           </div>
         </h1>
         <div class="w-full flex items-center justify-between max-md:flex-col max-md:gap-12 dark:text-text-muted font-lato">
-          <a class="text-nowrap" href="">🏠 Stuttgart, Germany.</a>
+          <a class="text-nowrap dark:hover:text-text" href="">🏠 Stuttgart, Germany.</a>
           <div class="w-full flex items-center justify-end max-md:justify-center gap-16 text-base dark:text-text-muted">
             <a href="" class="flex gap-8 items-center dark:hover:text-text">
               <i class="devicon-linkedin-plain colored text-lg"></i>
