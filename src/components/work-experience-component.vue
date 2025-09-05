c919977acc73b45975465c2a8baf3226fa374834
Felix Kuhbier <fkuhbier@stud.hs-heilbronn.de>
2025-09-05 03:51:02 +0200

    feat: add work experience component with details and description

---
+++ src/components/work-experience-component.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <section class="mt-100">
+    <h2 class="font-bold text-lg tracking-widest font-rubik uppercase mb-40">
+      Work Experience
+    </h2>
+    <div class="flex flex-col w-full gap-28">
+      <div class="flex w-full gap-16">
+        <p class="dark:text-text-muted whitespace-nowrap max-sm:hidden">
+          2023 - 2025
+        </p>
+        <div class="flex flex-col">
+          <p class="dark:text-text text-lg font-semibold font-lato">
+            Frontend Software Developer
+          </p>
+          <p class="dark:text-text-muted font-lato mb-16">
+            Schmalz GmbH • working student
+            <span class="dark:text-text-muted whitespace-nowrap hidden max-sm:inline-block">• (2023 - 2025)</span>
+          </p>
+          <p class="dark:text-text-muted font-lato">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos quis porro perspiciatis dolorem laudantium. Excepturi totam dignissimos quidem facilis pariatur odio omnis at deserunt, labore tempore dolores, esse quae est!
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
