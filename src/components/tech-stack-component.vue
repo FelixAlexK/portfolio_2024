04684075ca533ed04b037a7340c2a9114791f0a0
Felix Kuhbier <fkuhbier@stud.hs-heilbronn.de>
2025-09-05 03:50:54 +0200

    feat: add tech stack component with technology icons and links

---
+++ src/components/tech-stack-component.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <section class="mt-100">
+    <h2 class="font-bold text-lg tracking-widest font-rubik uppercase mb-40">
+      Tech Stack
+    </h2>
+    <div class="dark:text-text grid grid-cols-5 gap-20 max-md:grid-cols-4 max-sm:grid-cols-3">
+      <a href="" class="border dark:border-border dark:border-t-highlight bg-gradient-to-b dark:from-bg dark:to-bg-dark gap-4 h-100  dark:text-text-muted rounded-lg p-12 flex flex-col items-center justify-center dark:hover:text-text transition-all ease-in-out group">
+        <i class="devicon-css3-plain colored text-4xl" title="CSS3"></i>
+        CSS
+      </a>
+      <a href="" class="border dark:border-border dark:border-t-highlight bg-gradient-to-b dark:from-bg dark:to-bg-dark gap-4 h-100  dark:text-text-muted rounded-lg p-12 flex flex-col items-center justify-center dark:hover:text-text transition-all ease-in-out group">
+        <i class="devicon-html5-plain colored text-4xl" title="HTML5"></i>
+        HTML
+      </a>
+      <a href="" class="border dark:border-border dark:border-t-highlight bg-gradient-to-b dark:from-bg dark:to-bg-dark gap-4 h-100  dark:text-text-muted rounded-lg p-12 flex flex-col items-center justify-center dark:hover:text-text transition-all ease-in-out group">
+        <i class="devicon-typescript-plain colored text-4xl" title="TypeScript"></i>
+        TypeScript
+      </a>
+      <a href="" class="border dark:border-border dark:border-t-highlight bg-gradient-to-b dark:from-bg dark:to-bg-dark gap-4 h-100  dark:text-text-muted rounded-lg p-12 flex flex-col items-center justify-center dark:hover:text-text transition-all ease-in-out group">
+        <i class="devicon-vuejs-plain colored text-4xl" title="Vue.js"></i>
+        Vue
+      </a>
+      <a href="" class="border dark:border-border dark:border-t-highlight bg-gradient-to-b dark:from-bg dark:to-bg-dark gap-4 h-100  dark:text-text-muted rounded-lg p-12 flex flex-col items-center justify-center dark:hover:text-text transition-all ease-in-out group">
+        <i class="devicon-tailwindcss-plain colored text-4xl" title="Tailwind CSS"></i>
+        Tailwind
+      </a>
+      <a href="" class="border dark:border-border dark:border-t-highlight bg-gradient-to-b dark:from-bg dark:to-bg-dark gap-4 h-100  dark:text-text-muted rounded-lg p-12 flex flex-col items-center justify-center dark:hover:text-text transition-all ease-in-out group">
+        <i class="devicon-astro-plain colored text-4xl" title="Astro"></i>
+        Astro
+      </a>
+    </div>
+  </section>
+</template>
