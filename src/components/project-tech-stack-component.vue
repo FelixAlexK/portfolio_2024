0e78d3f64659a7e2b7bba8188f100a9b237339a0
Felix Kuhbier <fkuhbier@stud.hs-heilbronn.de>
2025-09-05 15:17:57 +0200

    feat: add project tech stack component with props for dynamic rendering

---
+++ src/components/project-tech-stack-component.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const { techName, bgColor, simpleIconName } = defineProps<{
+  techName: string;
+  bgColor: string;
+  simpleIconName?: string;
+}>();
+</script>
+
+<template>
+  <div
+    class="flex items-center justify-center gap-12 w-full px-16 h-28 rounded-full "
+    :style="{ backgroundColor: `${bgColor}33` }"
+  >
+    <slot v-if="$slots.icon" name="icon">
+    </slot>
+    <img v-else class="size-16" :src="`https://cdn.simpleicons.org/${simpleIconName ? simpleIconName : techName.toLowerCase()}`" />
+    <p class="text-xs whitespace-nowrap">
+      {{ techName }}
+    </p>
+  </div>
+</template>
